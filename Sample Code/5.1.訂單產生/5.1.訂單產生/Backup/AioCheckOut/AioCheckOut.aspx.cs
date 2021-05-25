c673404c01b733191590f2c36a2109d8a3b6beb9
DING-NB\Ding <andy19900208@gmail.com>
2021-05-25 20:03:02 +0800

    test by andy

--- Sample Code/5.1.訂單產生/5.1.訂單產生/Backup/AioCheckOut/AioCheckOut.aspx.cs
+++ Sample Code/5.1.訂單產生/5.1.訂單產生/Backup/AioCheckOut/AioCheckOut.aspx.cs
@@ -23,26 +23,19 @@ namespace AioCheckOut
                     /* 服務參數 */
                     oPayment.ServiceMethod = HttpMethod.HttpPOST;//介接服務時，呼叫 API 的方法
                     oPayment.ServiceURL = "https://payment-stage.ecpay.com.tw/Cashier/AioCheckOut/V5";//要呼叫介接服務的網址
-
                     oPayment.HashKey = "5294y06JbISpM5x9";//ECPay提供的Hash Key
                     oPayment.HashIV = "v77hoKGq4kWxNNIS";//ECPay提供的Hash IV
                     oPayment.MerchantID = "2000132";//ECPay提供的特店編號
-
-
-
 
                     /* 基本參數 */
                     oPayment.Send.ReturnURL = "http://example.com";//付款完成通知回傳的網址
                     oPayment.Send.ClientBackURL = "http://www.ecpay.com.tw/";//瀏覽器端返回的廠商網址
                     oPayment.Send.OrderResultURL = "http://localhost:52413/CheckOutFeedback.aspx";//瀏覽器端回傳付款結果網址
-
-                    //oPayment.Send.MerchantTradeNo = "ECPay" + new Random().Next(0, 99999).ToString();//廠商的交易編號
-                    oPayment.Send.MerchantTradeNo = "TestByAndy" + new Random().Next(0, 99999).ToString();//廠商的交易編號
-
+                    oPayment.Send.MerchantTradeNo = "ECPay" + new Random().Next(0, 99999).ToString();//廠商的交易編號
                     oPayment.Send.MerchantTradeDate = DateTime.Now.ToString("yyyy/MM/dd HH:mm:ss");//廠商的交易時間
-                    oPayment.Send.TotalAmount = Decimal.Parse("5000");//交易總金額
-                    oPayment.Send.TradeDesc = "ecpay 商城購物 ";//交易描述
-                    oPayment.Send.ChoosePayment = PaymentMethod.Credit;//使用的付款方式
+                    oPayment.Send.TotalAmount = Decimal.Parse("3280");//交易總金額
+                    oPayment.Send.TradeDesc = "交易描述";//交易描述
+                    oPayment.Send.ChoosePayment = PaymentMethod.ALL;//使用的付款方式
                     oPayment.Send.Remark = "";//備註欄位
                     oPayment.Send.ChooseSubPayment = PaymentMethodItem.None;//使用的付款子項目
                     oPayment.Send.NeedExtraPaidInfo = ExtraPaymentInfo.Yes;//是否需要額外的付款資訊
@@ -55,27 +48,15 @@ namespace AioCheckOut
                     oPayment.Send.CustomField4 = "";
                     oPayment.Send.EncryptType = 1;
 
-                    string PaymentType = oPayment.Send.PaymentType;
-
                     //訂單的商品資料
                     oPayment.Send.Items.Add(new Item()
                     {
                         Name = "蘋果",//商品名稱
-                        Price = Decimal.Parse("2500"),//商品單價
+                        Price = Decimal.Parse("3280"),//商品單價
                         Currency = "新台幣",//幣別單位
                         Quantity = Int32.Parse("1"),//購買數量
                         URL = "http://google.com",//商品的說明網址
-
-                    });
-
-                    oPayment.Send.Items.Add(new Item()
-                    {
-                        Name = "BANANA",//商品名稱
-                        Price = Decimal.Parse("500"),//商品單價
-                        Currency = "新台幣",//幣別單位
-                        Quantity = Int32.Parse("5"),//購買數量
-                        URL = "http://google.com",//商品的說明網址
-
+                        
                     });
 
                     /*************************非即時性付款:ATM、CVS 額外功能參數**************/
@@ -115,7 +96,7 @@ namespace AioCheckOut
 
                     //oPayment.SendExtend.Redeem = false;   //是否使用紅利折抵
                     //oPayment.SendExtend.UnionPay = true; //是否為銀聯卡交易
-
+                    
                     #endregion
 
                     #region 分期付款
@@ -133,7 +114,7 @@ namespace AioCheckOut
                     //oPayment.SendExtend.PeriodReturnURL = "";//伺服器端回傳定期定額的執行結果網址。
 
                     #endregion
-
+ 
                     /* 產生訂單 */
                     enErrors.AddRange(oPayment.CheckOut());
                 }
